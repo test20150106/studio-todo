556ea65299a6384d9210681dd511f67367cd4e2f
test20150106 <noreply>
2015-01-06 19:37:09 +0000

    Add component my-component-2

---
+++ ui/my-component-2.reel/my-component-2.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/my-component-2.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class MyComponent-2
+ * @extends Component
+ */
+exports.MyComponent-2 = Component.specialize(/** @lends MyComponent-2# */ {
+    constructor: {
+        value: function MyComponent-2() {
+            this.super();
+        }
+    }
+});
